a94fb2375c3b0ded328c6b7b521d1d4d8bb403e7
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-19 20:20:26 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,3 +9,7 @@ Script by:      https://github.com/bonben365
 For detailed script execution: https://bonben365.com/
 =================================================================================================================
 #>
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    break
+}
